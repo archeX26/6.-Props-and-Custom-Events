e8037529287fd9528b80a2b27d80c28cd8e1d540
archex26 <george.archaia@gmail.com>
2022-12-02 17:36:31 +0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <section>
+    <active-user :username='user.name' :userage='user.age'> </active-user>
+    <user-data @add-user="addUserData"></user-data>
+  </section>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            user:{
+                name:'giorgi',
+                age: 20,
+            }
+            
+        }
+    },
+    methods:{
+        addUserData(name,age) {
+      this.user={
+        name: name,
+        age: age
+      }
+      
+    },
+
+    }
+};
+</script>
+<style>
+html {
+  font-family: sans-serif;
+}
+
+section {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  border: 1px solid #ccc;
+  padding: 1rem;
+}
+
+</style>
---
+++ src/components/UserData.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <form @submit.prevent="submitData">
+      <input type="text" placeholder="name" v-model="userName" />
+      <input type="text" placeholder="age" v-model="userAge" />
+      <button>set data</button>
+    </form>
+  </section>
+</template>
+<script>
+export default {
+  emits: ['add-user'],
+  data() {
+    return {
+      userName: "",
+      userAge: "",
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit('add-user', this.userName, this.userAge);
+    },
+  },
+};
+</script>
